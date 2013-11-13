835dfb2a4d083ca37c05b88c6041c2ff79630438
Rossitsa Borissova rossitsa.borissova@skrill.com <rossitsaborissova@RBorissovaMob.sof.moneybookers.net>
2013-11-13 18:25:51 +0200

    [Task MBD-1] Initial changes

---
+++ src/main/java/com/skrill/interns/Hello.java
@@ -0,0 +1,17 @@
+package com.skrill.interns;
+
+import java.io.IOException;
+
+public class Hello {
+
+    public static void main (String [] s) {
+        byte [] name = new byte [256];
+        try {
+            System.in.read(name);
+            System.out.println("Hello, " + new String(name));
+        } catch (IOException e) {
+            System.out.println("cannot read input, " + e.getMessage());
+        }
+    }
+
+}
